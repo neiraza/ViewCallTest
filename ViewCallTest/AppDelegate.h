5cbc9ebec8678270d648a803a4422383d31fad8f
togu <tooru.oguri@gmail.com>
2015-03-19 02:23:34 +0900

    Initial Commit

---
+++ ViewCallTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ViewCallTest
+//
+//  Created by togu on 2015/03/19.
+//  Copyright (c) 2015年 togu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ViewCallTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ViewCallTest
+//
+//  Created by togu on 2015/03/19.
+//  Copyright (c) 2015年 togu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
